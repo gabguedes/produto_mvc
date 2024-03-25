53471c82995d6f5c42e4854f8164eb34f0670ecd
Gabriel Guedes <gabriel4g.crispim@gmail.com>
2024-03-25 11:27:11 -0300

    Implementação categoria e produto mvc

---
+++ src/main/java/br/com/fiap/produtomvc/controllers/CategoriaController.java
@@ -0,0 +1,84 @@
+package br.com.fiap.produtomvc.controllers;
+
+import br.com.fiap.produtomvc.models.Categoria;
+import br.com.fiap.produtomvc.repository.CategoriaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    @GetMapping("form")
+    public String loadForm(Model model){
+        model.addAttribute("categoria", new Categoria());
+        return "produto/nova-categoria";
+    }
+
+    @PostMapping()
+    @Transactional
+    public String insert(@Valid Categoria categoria,
+                         BindingResult br,
+                         RedirectAttributes attributes){
+        if(br.hasErrors()){
+            return "produto/nova-categoria";
+        }
+        repository.save(categoria);
+        attributes.addFlashAttribute("mensagem", "Categoria salva com sucesso!");
+        return "redirect:/categorias";
+    }
+
+    @GetMapping()
+    @Transactional(readOnly = true)
+    public String findAll(Model model){
+        model.addAttribute("categorias", repository.findAll());
+        return "produto/listar-categoria";
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public String findById(@PathVariable ("id") Long id, Model model){
+        Categoria categoria = repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Categoria Inválida - id: " + id)
+        );
+        model.addAttribute("categoria", categoria);
+        return "produto/editar-categoria";
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public String update(@PathVariable("id") Long id,
+                         @Valid Categoria categoria,
+                         BindingResult br){
+        if(br.hasErrors()){
+            categoria.setId(id);
+            return "/produto/editar-categoria";
+        }
+        repository.save(categoria);
+        return "redirect:/categorias";
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public String delete (@PathVariable("id") Long id, Model model){
+        if(!repository.existsById(id)){
+            throw new IllegalArgumentException("Categoria inválida - id: " + id);
+        }
+        try{
+          repository.deleteById(id);
+        }catch (Exception e){
+            throw new IllegalArgumentException("Categoria inválida - id: " + id);
+        }
+
+        return "redirect:/categorias";
+    }
+}
---
+++ src/main/java/br/com/fiap/produtomvc/models/Categoria.java
@@ -0,0 +1,66 @@
+package br.com.fiap.produtomvc.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    //atributos
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message =" campo requerido!")
+    @Size(min = 3, message = "O nome deve ter nome mínimo 3 caracteres.")
+    private String nome;
+
+    //construtor
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria() {}
+
+    //getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //equals & hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id == categoria.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
