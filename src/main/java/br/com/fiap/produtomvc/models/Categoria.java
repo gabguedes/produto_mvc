3d97faf89d4ce1e48c5610cf43688bd27eb44d16
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-01 11:37:26 -0300

    implementando driver oracle e lombok

--- src/main/java/br/com/fiap/produtomvc/models/Categoria.java
+++ src/main/java/br/com/fiap/produtomvc/models/Categoria.java
@@ -3,74 +3,34 @@ package br.com.fiap.produtomvc.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+//anotações javaBeans
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"nome"})
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
 
     //atributos
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+//    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_seq")
+    @SequenceGenerator(name = "categoria_seq", sequenceName = "categoria_seq", allocationSize = 1)
     private Long id;
 
     @NotBlank(message =" campo requerido!")
     @Size(min = 3, message = "O nome deve ter nome mínimo 3 caracteres.")
+    @Column(length = 150, nullable = false)
     private String nome;
 
     @OneToMany(mappedBy = "categoria")
     private List<Produto> produtos = new ArrayList<>();
-
-    //construtor
-    public Categoria(String nome) {
-        this.nome = nome;
-    }
-
-    public Categoria() {}
-
-    //getters & setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Produto> getProdutos() {
-        return produtos;
-    }
-
-    //equals & hashcode
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return id == categoria.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Categoria{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                '}';
-    }
 }
