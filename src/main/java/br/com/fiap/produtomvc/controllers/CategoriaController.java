262bbd7d0722ed43210d691de1e2a4e63953c7c8
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-03 09:37:26 -0300

    implementação da camada service

--- src/main/java/br/com/fiap/produtomvc/controllers/CategoriaController.java
+++ src/main/java/br/com/fiap/produtomvc/controllers/CategoriaController.java
@@ -1,7 +1,7 @@
 package br.com.fiap.produtomvc.controllers;
 
 import br.com.fiap.produtomvc.models.Categoria;
-import br.com.fiap.produtomvc.repository.CategoriaRepository;
+import br.com.fiap.produtomvc.services.CategoriaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +11,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/categorias")
 public class CategoriaController {
 
     @Autowired
-    private CategoriaRepository repository;
+    private CategoriaService service;
 
     @GetMapping("form")
     public String loadForm(Model model){
@@ -32,30 +34,26 @@ public class CategoriaController {
         if(br.hasErrors()){
             return "categoria/nova-categoria";
         }
-        repository.save(categoria);
+        categoria = service.insert(categoria);
         attributes.addFlashAttribute("mensagem", "Categoria salva com sucesso!");
         return "redirect:/categorias";
     }
 
     @GetMapping()
-    @Transactional(readOnly = true)
     public String findAll(Model model){
-        model.addAttribute("categorias", repository.findAll());
+        List<Categoria> categorias = service.findAll();
+        model.addAttribute("categorias", categorias);
         return "categoria/listar-categoria";
     }
 
     @GetMapping("/{id}")
-    @Transactional(readOnly = true)
     public String findById(@PathVariable ("id") Long id, Model model){
-        Categoria categoria = repository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("Categoria Inválida - id: " + id)
-        );
+        Categoria categoria = service.findById(id);
         model.addAttribute("categoria", categoria);
         return "categoria/editar-categoria";
     }
 
     @PutMapping("/{id}")
-    @Transactional
     public String update(@PathVariable("id") Long id,
                          @Valid Categoria categoria,
                          BindingResult br){
@@ -63,22 +61,13 @@ public class CategoriaController {
             categoria.setId(id);
             return "/categoria/editar-categoria";
         }
-        repository.save(categoria);
+        service.update(id, categoria);
         return "redirect:/categorias";
     }
 
     @DeleteMapping("/{id}")
-    @Transactional
     public String delete (@PathVariable("id") Long id, Model model){
-        if(!repository.existsById(id)){
-            throw new IllegalArgumentException("Recurso inválido - id: " + id);
-        }
-        try{
-          repository.deleteById(id);
-        }catch (Exception e){
-            throw new IllegalArgumentException("Falha na integridade referencial - id: " + id);
-        }
-
+       service.delete(id);
         return "redirect:/categorias";
     }
 }
