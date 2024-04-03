871cb7ae6fcdf0073a8a7679948e23e6a9355899
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-03 08:58:34 -0300

    reimplementando o h2db

--- src/main/java/br/com/fiap/produtomvc/models/Categoria.java
+++ src/main/java/br/com/fiap/produtomvc/models/Categoria.java
@@ -1,13 +1,22 @@
 package br.com.fiap.produtomvc.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 //anotações javaBeans
 @AllArgsConstructor
@@ -21,14 +30,14 @@ public class Categoria {
 
     //atributos
     @Id
-//    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_seq")
-    @SequenceGenerator(name = "categoria_seq", sequenceName = "categoria_seq", allocationSize = 1)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_seq")
+//    @SequenceGenerator(name = "categoria_seq", sequenceName = "categoria_seq", allocationSize = 1)
     private Long id;
 
     @NotBlank(message =" campo requerido!")
     @Size(min = 3, message = "O nome deve ter nome mínimo 3 caracteres.")
-    @Column(length = 150, nullable = false)
+//    @Column(length = 150, nullable = false)
     private String nome;
 
     @OneToMany(mappedBy = "categoria")
--- src/main/java/br/com/fiap/produtomvc/models/Produto.java
+++ src/main/java/br/com/fiap/produtomvc/models/Produto.java
@@ -1,13 +1,21 @@
 package br.com.fiap.produtomvc.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 // anotações javaBeans
 @AllArgsConstructor
@@ -20,18 +28,18 @@ import java.util.Objects;
 public class Produto {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto_seq")
-    @SequenceGenerator(name = "produto_seq", sequenceName = "produto_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto_seq")
+//    @SequenceGenerator(name = "produto_seq", sequenceName = "produto_seq", allocationSize = 1)
     private Long id;
 
     @NotBlank(message = "Campo requerido")
     @Size(min = 3, message = "O nome deve ter no mínimo 3 carateres")
-    @Column(length = 150, nullable = false)
+//    @Column(length = 150, nullable = false)
     private String nome;
 
     @NotBlank(message = "Campo requerido")
-    @Column(length = 255, nullable = false)
+//    @Column(length = 255, nullable = false)
     private String descricao;
 
     @NotNull(message = "Campo requerido")
